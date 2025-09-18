b84f9c0e06f37136a1968515651b958f7c784427
amorgrace <akanforte@gmail.com>
2025-09-18 01:04:59 +0100

    odd

--- chat/settings.py
+++ chat/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     'corsheaders.middleware.CorsMiddleware'
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 REST_AUTH_SERIALIZERS = {
@@ -149,6 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
